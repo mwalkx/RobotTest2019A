2f313b1ae8c7e19005917d0404e330c54ea8cb7b
Mark <walkx5m@sbcglobal.net>
2019-01-25 11:54:22 -0500

    Added IMU ADIS16448 library

--- src/main/java/frc/robot/subsystems/DriveTrainA.java
+++ src/main/java/frc/robot/subsystems/DriveTrainA.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Spark;
+import com.analog.adis16448.frc.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
